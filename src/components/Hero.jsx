28d0371af39319ab971548b2842c8c5f63dbf575
Nam Tang <nam.tang@live.fr>
2022-11-18 17:28:13 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="">
+        <h1 className="uppercase text-center font-bold text-white bg-blue-400 text-xl">We are creatives</h1>
+      <img src="\images\desktop\image-header.jpg" alt="" />
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-400 flex justify-between px-4 py-3">
+      <button>
+        <img src="\images\logo.svg" alt="" />
+      </button>
+      <button>
+        <img src="\images\icon-hamburger.svg" alt="" />
+      </button>
+    </nav>
+  );
+}
