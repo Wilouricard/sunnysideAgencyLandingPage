e7cac38d42aed6c87336bf573e94e2ad73ac6a4c
Nam Tang <nam.tang@live.fr>
2022-11-22 11:03:28 +0100

    change testimonials profile pictures layout

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -3,14 +3,10 @@ import React from "react";
 export default function Client(props) {
   return (
     <div className="text-center flex flex-col gap-6 px-16 pb-20">
-      <img
-        src={props.image}
-        alt=""
-        className="rounded-full w-1/4 mr-auto ml-auto"
-      />
-      <p>
-        {props.text}
-      </p>
+      <div className="w-[90px] h-[90px] rounded-full mr-auto ml-auto">
+        <img src={props.image} alt="" className="rounded-full" />
+      </div>
+      <p>{props.text}</p>
       <div>
         <h1>{props.who}</h1>
         <p>{props.position}</p>
